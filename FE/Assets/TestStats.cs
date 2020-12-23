9590695cd344c2f1ae023b37407fa6faee135e7a
Varpreet Dhaliwal <vdhaliwal@hotmail.com>
2020-12-22 23:38:53 -0800

    Stamina + Movement
    Stamina now impacts character movement

--- FE/Assets/TestStats.cs
+++ FE/Assets/TestStats.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 
 public class TestStats : MonoBehaviour
 {
+    public int base_mvnt;
     public int mvnt;
     public int strength;
     public int stamina;
@@ -20,18 +21,28 @@ public class TestStats : MonoBehaviour
     public GameObject ST_mask;
     public GameObject bar_instance;
     public bool bar_flag = false;
+    public Vector3 prev_position;
+    public int stamina_timer;
 
     // Start is called before the first frame update
     void Start()
     {
         cursor = GameObject.Find("Cursor");
         bar = GameObject.Find("Health and Stamina Bar");
+        prev_position = transform.position;
+        stamina_timer = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
         Vector3 pos = transform.position;
+        if (pos != prev_position)
+        {
+            stamina -= System.Convert.ToInt32(distance(pos, prev_position)*5/System.Convert.ToDouble(base_mvnt));
+            prev_position = pos;
+        }
+
         if (isOverlap(pos) & !bar_flag)
         {
             pos[1] += 1;
@@ -52,6 +63,7 @@ public class TestStats : MonoBehaviour
             }
             bar_flag = false;
         }
+        staminaUpdate();
     }
 
     bool isOverlap(Vector3 pos){
@@ -79,5 +91,29 @@ public class TestStats : MonoBehaviour
 
             transform.position = new Vector3(100, 100, 0);
         }
+    }
+
+    int distance(Vector3 pos1, Vector3 pos2)
+    {
+        return System.Convert.ToInt32(Math.Sqrt(Math.Pow(pos1[0] - pos2[0], 2) + Math.Pow(pos1[1] - pos2[1], 2)));
+    }
+
+    void staminaUpdate()
+    {
+        if (stamina < base_stamina)
+        {
+            stamina_timer += 1;
+            if (stamina_timer >= 1200)
+            {
+                stamina += 1;
+                stamina_timer -= 1200;
+            }
+        }
+        movementUpdate();
+    }
+
+    void movementUpdate()
+    {
+        mvnt = System.Convert.ToInt32(base_mvnt * (System.Convert.ToDouble(stamina) / System.Convert.ToDouble(base_stamina)));
     }
 }
